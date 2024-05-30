32a7be6c587bac08edb2b1d1e565c1246581c54c
luckyTomato <pearhill@163.com>
2024-05-31 01:26:15 +0800

    init with clang
    Signed-off-by: luckyTomato <pearhill@163.com>

---
+++ study/programming/demo_ion/producer/ionBufferManager.cpp
@@ -0,0 +1,5 @@
+#include "ionBufferManager.h"
+
+ionBufferManager::ionBUfferManager() {}
+
+ionBufferManager::~ionBUfferManager() {}
---
+++ study/programming/demo_ion/producer/ionBufferManager.h
@@ -0,0 +1,9 @@
+#ifndef ION_BUFFER_MANAGER_H
+#define _ION_BUFFER_MANAGER
+
+class ionBufferManager {
+public:
+    ionBufferManager();
+    ~ionBufferManager();
+};
+#endif
